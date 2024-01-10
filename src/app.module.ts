53757e333e6f61795f44a488e327f44255e85d93
Lucas Miiller <lucasmiillerbtc@gmail.com>
2024-01-10 11:27:01 -0300

    Adicionar configuração do banco de dados e .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from './infrastructure/database/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot(getConfig()),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/infrastructure/database/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export function getConfig(): TypeOrmModuleOptions {
+  const isProduction = process.env.NODE_ENV === 'production';
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'user',
+    password: process.env.DB_PASSWORD || 'pass',
+    database: process.env.DB_NAME || 'db',
+    synchronize: !isProduction,
+  };
+}
